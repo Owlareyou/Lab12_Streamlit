5d549eac1014941974e3e040209900080d2ce481
Jing Chuang <58900575+Owlareyou@users.noreply.github.com>
2024-04-10 12:13:52 -0700

    Add files via upload

---
+++ lab12_app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 12:09:03 2024
+
+@author: jing
+"""
+import streamlit as st
+import os
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+st.write("Lab_12: April 10")
